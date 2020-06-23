a9a9c2621716e8cf0042131d30a4589be2105cf1
karlmiche <karlymism@gmail.com>
2020-06-23 07:43:57 -0700

    stubbed out routes, remembered gitignore

--- controllers/cats.js
+++ controllers/cats.js
@@ -3,10 +3,83 @@ let router = express.Router()
 let fs = require('fs')
 
 // Index
+router.get("/", (req, res) => {
+    let cats = fs.readFileSync("./cats.json");
+
+    let catData = JSON.parse(cats);
+
+    let nameFiler = req.query.nameFilter
+
+    if(nameFilter){
+        catData = catData.filter(cat => {
+            return cat.name.toLowerCase() === nameFilter.toLowerCase()
+        })
+    }
+    res.render("cats/index", {myCats: catData})
+})
 
 // Show/Details
+// Get route to view *ONE* cat's information
+router.get('/:idx', (req, res) => {
+    let cats = fs.readFileSync('./cats.json')
+    let catData = JSON.parse(cats)
+    let catIndex = parseInt(req.params.idx)
+
+    res.render('cats/show', {myCat: catData[catIndex]})
+})
+
 // Add
+// Get route to view new cats form
+router.get("/new", (req, res) => {
+    res.render('cats/new')
+})
+
 // Create
+router.post("/", (req, res) => {
+    // read cats file
+    let cats = fs.readFileSync("./cats.json")
+    cats = JSON.parse(cats)
+    // add the new content to the cats array
+    cats.push(req.body)
+    // save the new array content to cats.json
+    fs.writeFileSync("./cats.json", JSON.stringify(cats))
+    // redirect to the /cats
+    res.redirect("/cats")
+})
+
 // Edit
+// Get route to view edit cats form
+router.get("/edit/:idx", (req, res) => {
+    let cats = fs.readFileSync("./cats.json")
+    cats = JSON.parse(cats)
+    res.render("cats/edit", {cat: cats[req.params.idx], catId: req.params.idx})
+})
+
 // Update
+router.put("/:idx", (req, res) => {
+    let cats = fs.readFileSync("./cats.json")
+    cats = JSON.parse(cats)
+    // Select name & type of cat selected by its ID, then reassign name & type
+    cats[req.params.idx].name = req.body.name
+    cats[req.params.idx].type = req.body.type
+    // rewrite the file
+    fs.writeFileSync("./cats.json", JSON.stringify(cats))
+    // redirect to main page
+    res.redirect("/cats")
+})
+
 // Destroy
+router.delete("/:idx", (req, res) => {
+    let cats = fs.readFileSync("./cats.json")
+    cats = JSON.parse(cats)
+    // Remove the selected cat from our "cats" array
+    cats.splice(req.params.idx, 1)
+    // Save over our cats.json with the newly formatted cats array.
+    fs.writeFileSync("./cats.json", JSON.stringify(cats))
+    // Once everything is done, we want to show the user the impact of their actions
+    // by redirecting to the /cats route to see all remaining cats.
+    res.redirect("/cats")
+})
+
+
+module.exports = router;
--- controllers/dogs.js
+++ controllers/dogs.js
@@ -3,9 +3,81 @@ let router = express.Router()
 let fs = require('fs')
 
 // Index
+router.get("/", (req, res) => {
+    let dogs = fs.readFileSync("./dogs.json");
+
+    let dogData = JSON.parse(dogs);
+
+    let nameFiler = req.query.nameFilter
+
+    if(nameFilter){
+        dogData = dogData.filter(dog => {
+            return dog.name.toLowerCase() === nameFilter.toLowerCase()
+        })
+    }
+    res.render("dogs/index", {myDogs: dogData})
+})
+
 // Show/Details
+router.get("/:idx", (req, res) => {
+    let dogs = fs.readFileSync("./dogs.json")
+    let dogData = JSON.parse(dogs)
+    let dogIndex = parseInt(req.params.idx)
+
+    res.render("dogs/show", {myDog: dogData[dogIndex]})
+})
+
 // Add
+// Get route to view new dog form
+router.get("/new", (req, res) => {
+    res.render("dogs/new")
+})
+
 // Create
+router.post("/", (req, res) => {
+    // read dogs file
+    let dogs = fs.readFileSync("./dogs.json")
+    dogs = JSON.parse(dogs)
+    // add the new content to the dogs array
+    dogs.push(req.body)
+    // save the new array content to dogs.json
+    fs.writeFileSync("./dogs.json", JSON.stringify(dogs))
+    // redirect to the /dogs
+    res.redirect("/dogs")
+})
+
 // Edit
+// Get route to view edit dogs form
+router.get("/edit/:idx", (req, res) => {
+    let dogs = fs.readFileSync("./dogs.json")
+    dogs = JSON.parse(dogs)
+    res.render("dogs/edit", {dog: dogs[req.params.idx], dogId: req.params.idx})
+})
+
 // Update
+router.put("/:idx", (req, res) => {
+    let dogs = fs.readFileSync("./dogs.json")
+    dogs = JSON.parse(dogs)
+    // Select name & type of dog selected by its ID, then reassign name & type
+    dogs[req.params.idx].name = req.body.name
+    dogs[req.params.idx].type = req.body.type
+    // rewrite the file
+    fs.writeFileSync("./dogs.json", JSON.stringify(dogs))
+    // redirect to main page
+    res.redirect("/dogs")
+})
+
 // Destroy
+router.delete("/:idx", (req, res) => {
+    let dogs = fs.readFileSync("./dogs.json")
+    dogs = JSON.parse(dogs)
+    // Remove the selected dog from our "dogs" array
+    dogs.splice(req.params.idx, 1)
+    // Save over our dogs.json with the newly formatted dogs array.
+    fs.writeFileSync("./dogs.json", JSON.stringify(dogs))
+    // Once everything is done, we want to show the user the impact of their actions
+    // by redirecting to the /dogs route to see all remaining dogs.
+    res.redirect("/dogs")
+})
+
+module.exports = router;
