24ed91894a0e463a1e6ccf60e3cf1a7d20694a9b
zbarovsky <zachary.barovsky@gmail.com>
2020-06-22 21:08:59 -0700

    app fully runs, bugs worked out as far as I've found. Need to add css and get images to load

--- controllers/cats.js
+++ controllers/cats.js
@@ -2,11 +2,12 @@ let express = require('express');
 let router = express.Router()
 let fs = require('fs');
 
+// route to show famous aniamls
 router.get('/', (req, res) => {
     let cats = fs.readFileSync('./cats.json');
     let catData = JSON.parse(cats);
     let nameFilter = req.query.nameFilter
-
+    // route to search for specific animal
     if (nameFilter) {
         catData = catData.filter(kitty => {
             return kitty.name.toLowerCase() === nameFilter.toLowerCase();
@@ -15,18 +16,54 @@ router.get('/', (req, res) => {
     res.render('cats/index', {myCat: catData})
 })
 
-// route to show famous aniamls
-
-// route to search for specific animal
-
 // route to get new animals
+router.get('/new', (req, res) => {
+    res.render('cats/new')
+})
 
 // route to edit current animals
+router.get('/edit/:idx', (req, res) => {
+    let cats = fs.readFileSync('./cats.json')
+    let catData = JSON.parse(cats)
+    res.render('cats/edit', {myCat: catData[req.params.idx], catId: req.params.idx})
+})
+
+router.get('/:idx', (req, res) => {
+    let cats = fs.readFileSync('./cats.json')
+    let catData = JSON.parse(cats)
+    let catIndex = parseInt(req.params.idx);
+
+    res.render('cats/show', {myCat: catData[catIndex]})
+})
 
 // route to get add/post new animals
+router.post('/', (req, res) => {
+    let cats = fs.readFileSync('./cats.json')
+    cats = JSON.parse(cats)
+    cats.push(req.body)
+    fs.writeFileSync('./cats.json', JSON.stringify(cats))
+    res.redirect('/cats')
+})
 
 // route to delete animals
+router.delete('/:idx', (req, res) => {
+    let cats = fs.readFileSync('./cats.json')
+    cats = JSON.parse(cats)
+    cats.splice(req.params.idx, 1)
+    fs.writeFileSync('./cats.json', JSON.stringify(cats))
+    res.redirect('/cats')
+})
 
 // route to update name and attributes of animals
+router.put('/:idx', (req, res) => {
+    let cats = fs.readFileSync('./cats.json');
+    cats = JSON.parse(cats);
+    cats[req.params.idx].name = req.body.name;
+    cats[req.params.idx].famousFor = req.body.famousFor;
+
+    fs.writeFileSync('./cats.json', JSON.stringify(cats))
+
+    res.redirect('/cats')
+})
 
 module.exports = router;
--- controllers/dogs.js
+++ controllers/dogs.js
@@ -1,6 +1,6 @@
 let express = require('express');
 let router = express.Router()
-let fs = require('fs');
+let fs = require('fs'); 
 
 // route to show famous aniamls
 
@@ -17,17 +17,52 @@ router.get('/', (req, res) => {
         res.render('dogs/index', {myDog: dogData});
 })
 
-
-
-
 // route to get new animals
+router.get('/new', (req, res) => {
+    res.render('dogs/new')
+})
 
 // route to edit current animals
+router.get('/edit/:idx', (req, res) => {
+    let dogs = fs.readFileSync('./dogs.json')
+    let dogData = JSON.parse(dogs)
+    res.render('dogs/edit', {myDog: dogData[req.params.idx], dogId: req.params.idx})
+})
 
-// route to get add/post new animals
+router.get('/:idx', (req, res) => {
+    let dogs = fs.readFileSync('./dogs.json')
+    let dogData = JSON.parse(dogs)
+    let dogIndex = parseInt(req.params.idx);
 
+    res.render('dogs/show', {myDog: dogData[dogIndex]})
+})
+
+// route to get show new animals
+router.post('/', (req, res) => {
+    let dogs = fs.readFileSync('./dogs.json')
+    dogs = JSON.parse(dogs)
+    dogs.push(req.body)
+    fs.writeFileSync('./dogs.json', JSON.stringify(dogs))
+    res.redirect('/dogs')
+})
 // route to delete animals
+router.delete('/:idx', (req, res) => {
+    let dogs = fs.readFileSync('./dogs.json')
+    dogs = JSON.parse(dogs)
+    dogs.splice(req.params.idx, 1)
+    fs.writeFileSync('./dogs.json', JSON.stringify(dogs))
+    res.redirect('/dogs')
+})
 
 // route to update name and attributes of animals
+router.put('/:idx', (req, res) => {
+    let dogs = fs.readFileSync('./dogs.json');
+    dogs = JSON.parse(dogs);
+    dogs[req.params.idx].name = req.body.name;
+    dogs[req.params.idx].famousFor = req.body.famousFor
 
+    fs.writeFileSync('./dogs.json', JSON.stringify(dogs))
+
+    res.redirect('/dogs')
+})
 module.exports = router;
