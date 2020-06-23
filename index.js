64363ca7081f61bedc9faf9dc181ab072799fc2a
Salman Malik <salmanmalik@Salmans-Air.attlocal.net>
2020-06-23 01:49:26 -0500

    Stubbed out the routes for cats, fleshing out my get routes now

--- index.js
+++ index.js
@@ -19,8 +19,63 @@ app.use(methodOverride('_method'));
 //adding the router route
 app.use('/dogs', require('./controllers/dogs'))
 
+
+
 app.get('/', (req, res) => {
     res.render('home')
+})
+
+
+//stubbing out index get route
+app.get('/cats', (req, res) => {
+   let cats = fs.readFileSync('./cats.json');
+   let catsData = JSON.parse(cats);
+   
+
+    res.render('cats/index', {myCats: catsData})
+})
+
+//stubbing out show/details route
+app.get('/cats/:idx', (req, res) => {
+    let cats = fs.readFileSync('./cats.json')
+    let catData = JSON.parse(cats);
+    //the index will be a string so we have to turn it into a num
+    catIndex = parseInt(req.params.idx)
+    
+    res.render('cats/show', {myCat: catData[catIndex]})
+})
+
+//stubbing out new route
+app.get('/cats/new', (req, res) => {
+    res.render('cats/new')
+})
+
+//stubbing out edit route
+app.get('/cats/edit/:idx', (req, res) => {
+    let cats = fs.readFileSync('./cats.json')
+    cats = JSON.parse(cats);
+
+    res.render('cats/edit', {})
+})
+
+//stubbing out the post/Create route
+app.post('/cats', (req, res) => {
+   console.log(req.body)
+   
+})
+
+//stubbing out the destroy/delete route
+app.delete('/cats/:idx', (req, res) => {
+    let cats = fs.readFileSync('./cats.json')
+    cats = JSON.parse(cats);
+
+})
+
+//stubbing out the update
+app.put('/cats/:idx', (req, res) => {
+    let cats = fs.readFileSync('./cats.json')
+    cats = JSON.parse(cats);
+
 })
 
 
