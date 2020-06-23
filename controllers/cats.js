12b0439463093b22708713b0359198df582bc09b
Weston Bailey <void@MBP.local>
2020-06-22 21:48:46 -0700

    stubbed views

--- controllers/cats.js
+++ controllers/cats.js
@@ -7,4 +7,18 @@ router.get('/', (request, response) => {
   response.render('cats/index');
 });
 
+router.get('/edit', (request, response) => {
+  response.render('cats/edit');
+});
+
+router.get('/new', (request, response) => {
+  response.render('cats/new');
+});
+
+router.get('/show', (request, response) => {
+  response.render('cats/show');
+});
+
+
+
 module.exports = router
--- controllers/dogs.js
+++ controllers/dogs.js
@@ -7,4 +7,16 @@ router.get('/', (request, response) => {
   response.render('dogs/index');
 });
 
+router.get('/edit', (request, response) => {
+  response.render('dogs/edit');
+});
+
+router.get('/new', (request, response) => {
+  response.render('dogs/new');
+});
+
+router.get('/show', (request, response) => {
+  response.render('dogs/show');
+});
+
 module.exports = router
