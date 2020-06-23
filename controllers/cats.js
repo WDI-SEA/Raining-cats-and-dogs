b597ad71529b5f01cb05ac519619e5777af04375
Weston Bailey <void@MBP.local>
2020-06-22 22:03:00 -0700

    stubbed all routes and views

--- controllers/cats.js
+++ controllers/cats.js
@@ -3,20 +3,44 @@ let router = express.Router()
 let fs = require('fs')
 
 
+//index route
 router.get('/', (request, response) => {
   response.render('cats/index');
 });
 
-router.get('/edit', (request, response) => {
-  response.render('cats/edit');
-});
-
+//new
 router.get('/new', (request, response) => {
   response.render('cats/new');
 });
 
-router.get('/show', (request, response) => {
+//edit
+router.get('/edit/:index', (request, response) => {
+  console.log(request.params.index)
+  response.render('cats/edit');
+});
+
+//show route from index
+router.get('/:index', (request, response) => {
+  console.log(request.params.index)
   response.render('cats/show');
+});
+
+//post form index
+router.post('/', (request, response) => {
+  console.log(req.body)
+  response.redirect('/cats')
+});
+
+//route for delete
+router.delete('/:index', (req, res) => {
+  console.log(request.params.body)
+  request.redirect('/cats')
+});
+
+//route for edits
+router.put('/:index', (req, res) => {
+  console.log(request.params.body)
+  request.redirect('/cats')
 });
 
 
--- controllers/dogs.js
+++ controllers/dogs.js
@@ -1,22 +1,48 @@
 let express = require('express')
 let router = express.Router()
-let fs = require('fs')
+let fs = require('fs');
+const { captureRejectionSymbol } = require('events');
+const { request } = require('http');
 
-
+//index route
 router.get('/', (request, response) => {
   response.render('dogs/index');
 });
 
-router.get('/edit', (request, response) => {
-  response.render('dogs/edit');
-});
-
+//new
 router.get('/new', (request, response) => {
   response.render('dogs/new');
 });
 
-router.get('/show', (request, response) => {
+//edit
+router.get('/edit/:index', (request, response) => {
+  console.log(request.params.index)
+  response.render('dogs/edit');
+});
+
+//show route from index
+router.get('/:index', (request, response) => {
+  console.log(request.params.index)
   response.render('dogs/show');
 });
+
+//post form index
+router.post('/', (request, response) => {
+  console.log(req.body)
+  response.redirect('/dogs')
+});
+
+//route for delete
+router.delete('/:index', (req, res) => {
+  console.log(request.params.body)
+  request.redirect('/dogs')
+});
+
+//route for edits
+router.put('/:index', (req, res) => {
+  console.log(request.params.body)
+  request.redirect('/dogs')
+});
+
 
 module.exports = router
